ee5ba3290f44a354497f63b91a0a263292726985
Thomas Blein <tblein@tblein.eu>
2018-04-14 17:01:40 +0200

    Simplification of modify
    remove updater and check broken check of installation variable

--- CRM/CivirulesAction/OVHMailingListApi/Modify.mgd.php
+++ CRM/CivirulesAction/OVHMailingListApi/Modify.mgd.php
@@ -1,20 +1,17 @@
 <?php
 
-if (_ovhmailinglistapi_modify_is_civirules_installed()) {
-  return array (
-    0 =>
-      array (
-        'name' => 'Civirules:Action.OVHMailingListApi.Modify',
-        'entity' => 'CiviRuleAction',
-        'params' =>
-          array (
-            'version' => 3,
-            'name' => 'ovhmailinglistapi_modify',
-            'label' => 'Modify OVH mailing list subscription',
-            'class_name' => 'CRM_CivirulesAction_OVHMailingListApi_Modify',
-            'is_active' => 1
-          ),
-      ),
-  );
-}
-else { return array(); }
+return array (
+  0 =>
+    array (
+      'name' => 'Civirules:Action.OVHMailingListApi.Modify',
+      'entity' => 'CiviRuleAction',
+      'params' =>
+        array (
+          'version' => 3,
+          'name' => 'ovhmailinglistapi_modify',
+          'label' => 'Modify OVH mailing list subscription',
+          'class_name' => 'CRM_CivirulesAction_OVHMailingListApi_Modify',
+          'is_active' => 1
+        ),
+    ),
+);
